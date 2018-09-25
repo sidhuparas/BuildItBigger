db9fe31aab352b02563b28eb8a0afae23712888d
Paras Sidhu <sidhuparas1234@gmail.com>
2018-09-25 19:11:34 +0530

    Add Emoji in JokeActivity

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -5,19 +5,14 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
-import com.parassidhu.javajokes.Joker;
 import com.parassidhu.jokemania.JokeActivity;
 import com.udacity.gradle.builditbigger.backend.myApi.MyApi;
 
@@ -33,28 +28,6 @@ public class MainActivity extends AppCompatActivity {
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void tellJoke(View view) {
@@ -87,7 +60,6 @@ class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
         }
 
         context = params[0];
-        //String name = params[0];
 
         try {
             return myApiService.getJoke().execute().getData();
@@ -98,8 +70,6 @@ class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
 
     @Override
     protected void onPostExecute(String result) {
-        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
-
         Intent intent = new Intent(context, JokeActivity.class);
         intent.putExtra(JokeActivity.JOKE_TEXT, result);
         context.startActivity(intent);
--- jokemania/src/main/java/com/parassidhu/jokemania/JokeActivity.java
+++ jokemania/src/main/java/com/parassidhu/jokemania/JokeActivity.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class JokeActivity extends AppCompatActivity {
 
@@ -25,10 +28,30 @@ public class JokeActivity extends AppCompatActivity {
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
                 WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
 
+        setData();
+    }
+
+    private void setData() {
         TextView jokeTv = findViewById(R.id.joke_tv);
+        ImageView emojiIv = findViewById(R.id.emoji_iv);
+
         if (getIntent() != null){
             String joke = getIntent().getStringExtra(JOKE_TEXT);
             jokeTv.setText(joke);
+        }
+
+        int randomEmoji = new Random().nextInt(3);
+
+        switch (randomEmoji){
+            case 0:
+                emojiIv.setImageDrawable(getResources().getDrawable(R.drawable.emoji_joy));
+                break;
+            case 1:
+                emojiIv.setImageDrawable(getResources().getDrawable(R.drawable.emoji_tounge));
+                break;
+            case 2:
+                emojiIv.setImageDrawable(getResources().getDrawable(R.drawable.emoji_floor));
+                break;
         }
     }
 }
